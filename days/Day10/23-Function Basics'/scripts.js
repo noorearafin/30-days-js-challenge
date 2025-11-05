efed48a19335383da156cafff7fd5745eaf7440e
noorearafin <noorearafin@gmail.com>
2025-11-05 18:38:59 +0600

    partiall upload

---
+++ days/Day10/23-Function Basics'/scripts.js
@@ -0,0 +1,15 @@
+//function
+
+function person(name, age) {
+    console.log("person name is" + name + "and age is" + age);
+}
+
+person('rafi', 20); //argument
+
+function phone(model, price) {
+    console.log(price, model);
+    return model, price;
+}
+
+phone('Iphone 13', 67000);
+
